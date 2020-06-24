7e6c10f6bd2849982c6692fa563bf5e06af9c966
hypoalien <hypoalien@gmail.com>
2020-06-24 23:22:39 +0530

    Added delete profile component

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -5,6 +5,7 @@ import {
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
   GET_ERRORS,
+  SET_CURRENT_USER,
 } from "./types";
 
 // Get current profile
@@ -37,6 +38,62 @@ export const createProfile = (profileData, history) => (dispatch) => {
         payload: err.response.data,
       })
     );
+};
+
+// Delete Experience
+export const deleteExperience = (id) => (dispatch) => {
+  axios
+    .delete(`/api/profile/experience/${id}`)
+    .then((res) =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+// Delete Education
+export const deleteEducation = (id) => (dispatch) => {
+  axios
+    .delete(`/api/profile/education/${id}`)
+    .then((res) =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+// Delete account & profile
+export const deleteAccount = () => (dispatch) => {
+  if (window.confirm("Are you sure? This can NOT be undone!")) {
+    axios
+      .delete("/api/profile")
+      .then((res) =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {},
+        })
+      )
+      .catch((err) =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data,
+        })
+      );
+  }
 };
 
 // Profile loading
