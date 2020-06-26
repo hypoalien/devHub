a25b1440a8d5f73acb78bb09f984b81b4aac56f6
hypoalien <hypoalien@gmail.com>
2020-06-26 17:42:25 +0530

    Added favicon

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,7 +3,8 @@ import React from "react";
 export default () => {
   return (
     <footer className="bg-dark text-white mt-5 p-4 text-center">
-      Copyright &copy; {new Date().getFullYear()} DevHub
+      Copyright &copy; {new Date().getFullYear()} DevHub DESIGNED AND DEVELOPED
+      BY HYPOALIEN
     </footer>
   );
 };
